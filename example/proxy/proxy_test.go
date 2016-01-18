81b4494cdcf57ba39cf1d41c389c8f9dca5989fa
Koala Yeung <koalay@gmail.com>
2016-01-18 17:24:51 +0800

    Refactor proxy example
    1. Remove obsoleted log message in proxy example.
    2. Relocate handler for better flexibility and readability.

--- example/proxy/proxy_test.go
+++ example/proxy/proxy_test.go
@@ -2,7 +2,6 @@ package proxy_test
 
 import (
 	"fmt"
-	"log"
 	"net"
 	"net/http"
 	"net/http/fcgi"
@@ -13,42 +12,49 @@ import (
 	"github.com/yookoala/gofast/example/proxy"
 )
 
-func app(network, address string) (l net.Listener, err error) {
+func app(network, address string, fn http.HandlerFunc) (l net.Listener, err error) {
 	l, err = net.Listen(network, address)
 	if err != nil {
 		return
 	}
-	handler := func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("in app")
-		fmt.Fprintf(w, "hello world")
-	}
-	go fcgi.Serve(l, http.HandlerFunc(handler))
+	go fcgi.Serve(l, http.HandlerFunc(fn))
 	return
 }
 
 func TestProxy(t *testing.T) {
+
+	// create temporary socket in the testing folder
 	dir, err := os.Getwd()
 	if err != nil {
 		t.Errorf("unexpected error: %#v", err.Error())
 	}
 	sock := dir + "/test.sock"
-	log.Printf(sock)
 
-	l, err := app("unix", sock)
+	// create temporary fcgi application server
+	// that listens to the socket
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		t.Logf("accessing FastCGI process")
+		fmt.Fprintf(w, "hello world")
+	}
+	l, err := app("unix", sock, fn)
 	if err != nil {
 		t.Errorf("unexpected error: %#v", err.Error())
 	}
 	defer os.Remove(sock)
 	defer l.Close()
 
+	// deine a proxy that access the temp fcgi application server
 	p := proxy.New(l.Addr().Network(), l.Addr().String())
 	w := httptest.NewRecorder()
+
+	// request the application server
 	r, err := http.NewRequest("GET", "/add", nil)
 	if err != nil {
 		t.Errorf("unexpected error: %#v", err.Error())
 	}
 	p.ServeHTTP(w, r)
 
+	// examine the result
 	if want, have := "hello world", w.Body.String(); want != have {
 		t.Errorf("expected %#v, got %#v", want, have)
 	}
