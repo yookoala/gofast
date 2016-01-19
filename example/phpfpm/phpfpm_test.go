93405317bdde8b5cd7818683afc2a67fe8aff2ba
Koala Yeung <koalay@gmail.com>
2016-01-19 19:39:55 +0800

    Fix example issue with Travis
    Use gophpfpm to handle php-fpm process to handle the php5-fpm issue.

--- example/phpfpm/phpfpm_test.go
+++ example/phpfpm/phpfpm_test.go
@@ -5,23 +5,20 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
-	"os/exec"
 	"path"
-	"regexp"
-	"syscall"
 	"testing"
-	"time"
 
-	"github.com/go-ini/ini"
+	"github.com/yookoala/gophpfpm"
 
 	"github.com/yookoala/gofast/example/phpfpm"
 )
 
-var phpfpmPath, phpfpmListen string
+var username, phpfpmPath, phpfpmListen string
 
 func init() {
 	phpfpmPath = os.Getenv("TEST_PHPFPM_PATH")
 	phpfpmListen = os.Getenv("TEST_PHPFPM_LISTEN")
+	username = os.Getenv("USER")
 }
 
 func isExamplePath(testPath string) bool {
@@ -63,47 +60,6 @@ func examplePath() string {
 	panic("example path not found")
 }
 
-func genConfig(basePath string) (confPath, network, address string) {
-
-	if basePath == "" {
-		panic("empty basePath")
-	}
-
-	// treat as tcp by default
-	network = "tcp"
-	address = phpfpmListen
-	listen := phpfpmListen
-
-	// check if use socket
-	if listen == "" {
-		network = "unix"
-		address = path.Join(basePath, "var", "phpfpm.sock")
-		listen = address
-	} else if reSocket := regexp.MustCompile("^(unix)\\:(.*)$"); reSocket.MatchString(listen) {
-		network = "unix"
-		address = reSocket.FindStringSubmatch(phpfpmListen)[1]
-		listen = address
-	}
-
-	confPath = path.Join(basePath, "etc", "phpfpm.conf")
-	pidfile := path.Join(basePath, "var", "phpfpm.pid")
-
-	cfg := ini.Empty()
-	cfg.NewSection("global")
-	cfg.Section("global").NewKey("pid", pidfile)
-	cfg.Section("global").NewKey("error_log", path.Join(basePath, "var", "phpfpm.error_log"))
-	cfg.NewSection("www")
-	cfg.Section("www").NewKey("listen", listen)
-	cfg.Section("www").NewKey("pm", "dynamic")
-	cfg.Section("www").NewKey("pm.max_children", "5")
-	cfg.Section("www").NewKey("pm.start_servers", "2")
-	cfg.Section("www").NewKey("pm.min_spare_servers", "1")
-	cfg.Section("www").NewKey("pm.max_spare_servers", "3")
-
-	cfg.SaveTo(confPath)
-	return
-}
-
 func TestHandler(t *testing.T) {
 
 	if phpfpmPath == "" {
@@ -119,41 +75,18 @@ func TestHandler(t *testing.T) {
 	}
 
 	exmpPath := examplePath()
-	confPath, network, address := genConfig(exmpPath)
-	cmd := &exec.Cmd{
-		Path: phpfpmPath,
-		Args: append([]string{phpfpmPath},
-			"--fpm-config", confPath,
-			"-F",  // start foreground
-			"-n",  // no ini file
-			"-e"), // extended information
-		Stderr: os.Stderr, // for now
-	}
-
-	stdoutRead, err := cmd.StdoutPipe()
-	if err != nil {
-		t.Errorf("unexpected error %v", err)
+	process := gophpfpm.NewProcess(phpfpmPath)
+	process.SetDatadir(path.Join(exmpPath, "var"))
+	process.User = username
+	process.SaveConfig(path.Join(exmpPath, "etc", "test.handler.conf"))
+	if err := process.Start(); err != nil {
+		t.Errorf("unexpected error: %s", err.Error())
 		return
 	}
-	defer stdoutRead.Close()
-
-	// start the command then wait to kill after test end
-	if err := cmd.Start(); err != nil {
-		t.Errorf("unexpected error %v", err)
-		return
-	}
-
-	// sleep a bit and wait for the process start
-	time.Sleep(time.Millisecond * 500)
-	defer cmd.Process.Wait()
-	defer cmd.Process.Signal(os.Interrupt)
-
-	t.Logf("started php-fpm on process %#v", cmd.Process.Pid)
-	if err := cmd.Process.Signal(syscall.Signal(0)); err != nil {
-		t.Logf("process.Signal on pid %d returned: %v\n", cmd.Process.Pid, err)
-	}
+	defer process.Stop()
 
 	// start the proxy handler
+	network, address := process.Address()
 	h := phpfpm.NewHandler(
 		path.Join(exmpPath, "htdocs"),
 		network, address)
