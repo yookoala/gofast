468021a06792e41fd48e7e8a59d4ad2c7dcce625
Koala Yeung <koalay@gmail.com>
2017-01-06 13:59:34 +0800

    godoc: improve comment formatting
    Fix minor formatting issues on godoc.org output.

--- session.go
+++ session.go
@@ -21,32 +21,31 @@ type SessionHandler func(client Client, req *Request) (resp *ResponsePipe, err e
 // the *Request, add extra business logic in between, rewrite the response
 // stream from *ResponsePipe. or better handle errors
 //
-// Ordinary fastcgi parameters:
+// Ordinary fastcgi parameters on nginx (for PHP at least):
 //
-// refer from nginx fastcgi_params
-// fastcgi_param  SCRIPT_FILENAME  $document_root$fastcgi_script_name;
-// fastcgi_split_path_info ^(.+\.php)(/?.+)$;
-// fastcgi_param PATH_INFO $fastcgi_path_info;
-// fastcgi_param PATH_TRANSLATED $document_root$fastcgi_path_info;
-// fastcgi_param  QUERY_STRING       $query_string;
-// fastcgi_param  REQUEST_METHOD     $request_method;
-// fastcgi_param  CONTENT_TYPE       $content_type;
-// fastcgi_param  CONTENT_LENGTH     $content_length;
-// fastcgi_param  SCRIPT_NAME        $fastcgi_script_name;
-// fastcgi_param  REQUEST_URI        $request_uri;
-// fastcgi_param  DOCUMENT_URI       $document_uri;
-// fastcgi_param  DOCUMENT_ROOT      $document_root;
-// fastcgi_param  SERVER_PROTOCOL    $server_protocol;
-// fastcgi_param  HTTPS              $https if_not_empty;
-// fastcgi_param  GATEWAY_INTERFACE  CGI/1.1;
-// fastcgi_param  SERVER_SOFTWARE    nginx/$nginx_version;
-// fastcgi_param  REMOTE_ADDR        $remote_addr;
-// fastcgi_param  REMOTE_PORT        $remote_port;
-// fastcgi_param  SERVER_ADDR        $server_addr;
-// fastcgi_param  SERVER_PORT        $server_port;
-// fastcgi_param  SERVER_NAME        $server_name;
-// # PHP only, required if PHP was built with --enable-force-cgi-redirect
-// fastcgi_param  REDIRECT_STATUS    200;
+//  fastcgi_split_path_info ^(.+\.php)(/?.+)$;
+//  fastcgi_param  SCRIPT_FILENAME    $document_root$fastcgi_script_name;
+//  fastcgi_param  PATH_INFO          $fastcgi_path_info;
+//  fastcgi_param  PATH_TRANSLATED    $document_root$fastcgi_path_info;
+//  fastcgi_param  QUERY_STRING       $query_string;
+//  fastcgi_param  REQUEST_METHOD     $request_method;
+//  fastcgi_param  CONTENT_TYPE       $content_type;
+//  fastcgi_param  CONTENT_LENGTH     $content_length;
+//  fastcgi_param  SCRIPT_NAME        $fastcgi_script_name;
+//  fastcgi_param  REQUEST_URI        $request_uri;
+//  fastcgi_param  DOCUMENT_URI       $document_uri;
+//  fastcgi_param  DOCUMENT_ROOT      $document_root;
+//  fastcgi_param  SERVER_PROTOCOL    $server_protocol;
+//  fastcgi_param  HTTPS              $https if_not_empty;
+//  fastcgi_param  GATEWAY_INTERFACE  CGI/1.1;
+//  fastcgi_param  SERVER_SOFTWARE    nginx/$nginx_version;
+//  fastcgi_param  REMOTE_ADDR        $remote_addr;
+//  fastcgi_param  REMOTE_PORT        $remote_port;
+//  fastcgi_param  SERVER_ADDR        $server_addr;
+//  fastcgi_param  SERVER_PORT        $server_port;
+//  fastcgi_param  SERVER_NAME        $server_name;
+//  # PHP only, required if PHP was built with --enable-force-cgi-redirect
+//  fastcgi_param  REDIRECT_STATUS    200;
 //
 type Middleware func(SessionHandler) SessionHandler
 
@@ -73,20 +72,20 @@ func BasicSession(client Client, req *Request) (*ResponsePipe, error) {
 // req.Params.
 //
 // Parameters included:
-//  * CONTENT_TYPE
-//  * CONTENT_LENGTH
-//  * HTTPS
-//  * GATEWAY_INTERFACE
-//  * REMOTE_ADDR
-//  * REMOTE_PORT
-//  * SERVER_PORT
-//  * SERVER_NAME
-//  * SERVER_PROTOCOL
-//  * SERVER_SOFTWARE
-//  * REDIRECT_STATUS
-//  * REQUEST_METHOD
-//  * REQUEST_URI
-//  * QUERY_STRING
+//  CONTENT_TYPE
+//  CONTENT_LENGTH
+//  HTTPS
+//  GATEWAY_INTERFACE
+//  REMOTE_ADDR
+//  REMOTE_PORT
+//  SERVER_PORT
+//  SERVER_NAME
+//  SERVER_PROTOCOL
+//  SERVER_SOFTWARE
+//  REDIRECT_STATUS
+//  REQUEST_METHOD
+//  REQUEST_URI
+//  QUERY_STRING
 //
 func BasicParamsMap(inner SessionHandler) SessionHandler {
 	return func(client Client, req *Request) (*ResponsePipe, error) {
@@ -150,12 +149,12 @@ type FileSystemRouter struct {
 // i.e. classic PHP hosting environment like Apache + mod_php
 //
 // Parameters included:
-//  * PATH_INFO
-//  * PATH_TRANSLATED
-//  * SCRIPT_NAME
-//  * SCRIPT_FILENAME
-//  * DOCUMENT_URI
-//  * DOCUMENT_ROOT
+//  PATH_INFO
+//  PATH_TRANSLATED
+//  SCRIPT_NAME
+//  SCRIPT_FILENAME
+//  DOCUMENT_URI
+//  DOCUMENT_ROOT
 //
 func (fs *FileSystemRouter) Router() Middleware {
 	return func(inner SessionHandler) SessionHandler {
@@ -191,12 +190,14 @@ func (fs *FileSystemRouter) Router() Middleware {
 	}
 }
 
-// MapHeader implement Middleware to map header field SomeRandomField to
+// MapHeader implement Middleware to map header field HTTP_*
+//
+// It is a convention to map header field SomeRandomField to
 // HTTP_SOME_RANDOM_FIELD. For example, if a header field "X-Hello-World" is in
 // the header, it will be mapped as "X_HELLO_WORLD" in the fastcgi parameter
 // field.
 //
-// CONTENT_TYPE and CONTENT_LENGTH cannot be overridden.
+// Note: HTTP_CONTENT_TYPE and HTTP_CONTENT_LENGTH cannot be overridden.
 //
 func MapHeader(inner SessionHandler) SessionHandler {
 	return func(client Client, req *Request) (*ResponsePipe, error) {
@@ -236,12 +237,12 @@ func MapHeader(inner SessionHandler) SessionHandler {
 // on its own). Suitable for web.py based application.
 //
 // Parameters included:
-//  * PATH_INFO
-//  * PATH_TRANSLATED
-//  * SCRIPT_NAME
-//  * SCRIPT_FILENAME
-//  * DOCUMENT_URI
-//  * DOCUMENT_ROOT
+//  PATH_INFO
+//  PATH_TRANSLATED
+//  SCRIPT_NAME
+//  SCRIPT_FILENAME
+//  DOCUMENT_URI
+//  DOCUMENT_ROOT
 //
 func MapEndpoint(endpointFile string) Middleware {
 	dir, webpath := filepath.Dir(endpointFile), "/"+filepath.Base(endpointFile)
