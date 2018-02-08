eae5af14d098d65d4785996526ededaf506e9048
Koala Yeung <koalay@gmail.com>
2018-02-08 12:55:46 +0800

    added stringer to recType for better debugging

--- fcgi.go
+++ fcgi.go
@@ -37,6 +37,41 @@ const (
 	typeUnknownType     recType = 11
 )
 
+// String implements fmt.Stringer
+func (t recType) String() string {
+	switch t {
+	case typeBeginRequest:
+		return "FCGI_BEGIN_REQUEST"
+	case typeAbortRequest:
+		return "FCGI_BEGIN_REQUEST"
+	case typeEndRequest:
+		return "FCGI_END_REQUEST"
+	case typeParams:
+		return "FCGI_PARAMS"
+	case typeStdin:
+		return "FCGI_STDIN"
+	case typeStdout:
+		return "FCGI_STDOUT"
+	case typeStderr:
+		return "FCGI_STDERR"
+	case typeData:
+		return "FCGI_DATA"
+	case typeGetValues:
+		return "FCGI_GET_VALUES"
+	case typeGetValuesResult:
+		return "FCGI_GET_VALUES_RESULT"
+	case typeUnknownType:
+		fallthrough
+	default:
+		return "FCGI_UNKNOWN_TYPE"
+	}
+}
+
+// GoString implements fmt.GoStringer
+func (t recType) GoString() string {
+	return t.String()
+}
+
 // keep the connection between web-server and responder open after request
 const flagKeepConn = 1
 
